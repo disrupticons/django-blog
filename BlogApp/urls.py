c1033c6e153a12801fd61c85bc56d010fa789dcb
disrupticons <disrupticons@gmail.com>
2015-09-11 19:11:34 +0530

    add user registration

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -15,14 +15,22 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView
 
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
-	url(r'^accounts/login', auth_views.login, {
+    url(r'^accounts/register/', CreateView.as_view(
+        template_name='register.html',
+        form_class=UserCreationForm,
+        success_url= '/blog/'
+    ), name='register'),
+	url(r'^accounts/login/', auth_views.login, {
         'template_name' : 'login.html'
     }, name='login'),
-	url(r'^accounts/logout', auth_views.logout_then_login, name='logout'),
+	url(r'^accounts/logout/', auth_views.logout_then_login, name='logout'),
 	url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^admin/', include(admin.site.urls)),
 ]
